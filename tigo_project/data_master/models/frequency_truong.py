e5ff293d73038f76ef9065db8225db66a0752d80
Sơn Vũ <vuthaison1705@gmail.com>
2023-02-09 20:52:01 +0700

    tigo_project

---
+++ tigo_project/data_master/models/frequency_truong.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, api, fields
+
+
+class FrequencyModels(models.Model):
+    _name = 'frequency.models'
+    _description = 'Tần suất'
+
+    name = fields.Char('Đơn vị')
+    numbers = fields.Integer('Số lần')
