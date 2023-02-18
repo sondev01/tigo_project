ab93018489e2496bd62395173407fbf20b314659
Sơn Vũ <vuthaison1705@gmail.com>
2023-02-18 14:43:56 +0700

    Danh mục mẫu sổ

--- data_master/models/applied_subjects.py
+++ data_master/models/applied_subjects.py
@@ -6,6 +6,7 @@ from odoo import models, api, fields
 class AppliedSubjects(models.Model):
     _name = 'applied.subjects'
     _description = 'Môn học áp dụng'
+    _rec_name = 'name_subjects'
 
     name_subjects = fields.Char(string="Tên môn học", required=True)
     code = fields.Char(string="Mã môn học", required=True)
