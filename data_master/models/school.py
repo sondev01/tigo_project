c538b2fd6fc07b9fd4cdc7f759c1c47491c3f58b
Pham Truong <tuantruongtb01@gmail.com>
2023-02-24 19:25:33 +0700

    update db

--- data_master/models/school.py
+++ data_master/models/school.py
@@ -7,3 +7,5 @@ class School(models.Model):
 
     name = fields.Char('Tên trường', required=True)
     code = fields.Char('Mã trường', required=True)
+    level_id = fields.Many2one('school.level', string='Cấp học', required=True)
+    edu_dp_id = fields.Many2one('education.models', string='Phòng giáo dục', required=True)
